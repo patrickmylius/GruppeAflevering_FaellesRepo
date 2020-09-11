d6e9d31e6d06fadb83c7e6e581ef726cade61276
Usamah Javaid <usamahjavaid@hotmail.com>
2020-09-11 11:59:55 +0200

    Jeg har lige tilføjet 2 sout

--- src/GruppeAflevering_FaellesRepo.java
+++ src/GruppeAflevering_FaellesRepo.java
@@ -1,5 +1,9 @@
 public class GruppeAflevering_FaellesRepo {
     public static void main(String[] args) {
+
         System.out.println("Hej Mikkel Her Igen");
+        System.out.println("Hej Usamah her");
+        System.out.println(" This is difficult ");
     }
+
 }
