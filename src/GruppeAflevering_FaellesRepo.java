3a697a136edafc79c9b2b2e2d2ba4205c07d9a3e
Danijel Gitanovic <dani025a@edu.ucl.dk>
2020-09-11 11:58:38 +0200

    Jeg har skrevet en kode

--- src/GruppeAflevering_FaellesRepo.java
+++ src/GruppeAflevering_FaellesRepo.java
@@ -1,5 +1,6 @@
 public class GruppeAflevering_FaellesRepo {
     public static void main(String[] args) {
         System.out.println("Hej Mikkel Her Igen");
+        System.out.println("Jeg vil gerne have fri nu! Hilsen Danijel");
     }
 }
