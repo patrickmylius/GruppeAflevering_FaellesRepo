c942488842775de4162890224644a7a8f1a5b074
patrickmylius <patrickmylius@outlook.dk>
2020-09-11 12:03:10 +0200

    Tilføjet metode.

--- src/GruppeAflevering_FaellesRepo.java
+++ src/GruppeAflevering_FaellesRepo.java
@@ -1,5 +1,20 @@
 public class GruppeAflevering_FaellesRepo {
     public static void main(String[] args) {
         System.out.println("Hej Mikkel Her Igen");
+        gruppeIntroduktion();
+    }
+    public static void gruppeIntroduktion(){
+        System.out.println("******* VI ER GRUPPE *******");
+        System.out.println("******* COPY PASTE   *******");
+        System.out.println("            ***             ");
+        System.out.println("            ***             ");
+        System.out.println("            ***             ");
+        System.out.println("            ***             ");
+        System.out.println("            ***             ");
+        System.out.println("       *************        ");
+        System.out.println("       *************        ");
+        System.out.println("       *************        ");
+        System.out.println("       *************        ");
+        System.out.println("       *************        ");
     }
 }
