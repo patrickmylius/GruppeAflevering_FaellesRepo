dcc71fdb53faecbf732a88a9617bd8ea0b8aad5c
patrickmylius <patrickmylius@outlook.dk>
2020-09-11 14:04:49 +0200

    Instantieret String variable
    Tilføjet loop
    
    Tilføjet metode gruppeIntroduktion();
    Tilføjet metode bevisPaaString(String navn)

--- src/GruppeAflevering_FaellesRepo.java
+++ src/GruppeAflevering_FaellesRepo.java
@@ -1,13 +1,28 @@
 public class GruppeAflevering_FaellesRepo {
+    //kode start
     public static void main(String[] args) {
+        //instans af variable (String)
+        String output = "KODNING GRUPPE OPGAVE";
 
-        System.out.println("Hej Mikkel Her Igen");
-        System.out.println("Jeg vil gerne have fri nu! Hilsen Danijel");
-        System.out.println("Hej Usamah her");
-        System.out.println(" This is difficult ");
+        //If loop. hvis output = "KODNING GRUPPE OPGAVE" køres loop og souts printes.
+        if (output == "KODNING GRUPPE OPGAVE") {
+            System.out.println("Hej Mikkel Her Igen");
+            System.out.println("Jeg vil gerne have fri nu! Hilsen Danijel");
+            System.out.println("Hej Usamah her");
+            System.out.println(" This is difficult ");
+        }
+        //Brug af metode som introducere gruppe
         gruppeIntroduktion();
-    }
 
+        //Brug af metode som tager imod et "navn"
+        bevisPaaString("Danijel");
+        bevisPaaString("Mikkel");
+        bevisPaaString("Daniella");
+        bevisPaaString("Usamah");
+        bevisPaaString("Patrick");
+
+    }
+    //Metode som printer souts.
     public static void gruppeIntroduktion() {
         System.out.println("******* VI ER GRUPPE *******");
         System.out.println("******* COPY PASTE   *******");
@@ -21,5 +36,9 @@ public class GruppeAflevering_FaellesRepo {
         System.out.println("       *************        ");
         System.out.println("       *************        ");
         System.out.println("       *************        ");
+    }
+    //metode, som tager imod (Tekst)
+    public static void bevisPaaString(String navn){
+        System.out.println(navn + " " + "er gået på weekend");
     }
 }
